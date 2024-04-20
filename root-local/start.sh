27d25359388e84d14ad2a7c3a0001aff84a26403
danveitch76 <45300973+danveitch76@users.noreply.github.com>
2024-04-20 08:05:29 +0100

    Add local self-host files
    Need to still change local asset location in config files.

---
+++ root-local/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Perform the initial configuration
+/bin/bash /init.sh
+
+echo "            _   _                 _                      "
+echo " _ __   ___| |_| |__   ___   ___ | |_  __  ___   _ ____  "
+echo "| '_ \ / _ \ __| '_ \ / _ \ / _ \| __| \ \/ / | | |_  /  "
+echo "| | | |  __/ |_| |_) | (_) | (_) | |_ _ >  <| |_| |/ /   "
+echo "|_| |_|\___|\__|_.__/ \___/ \___/ \__(_)_/\_\\__,  /___| "
+echo "                                             |___/       "
+
+/usr/bin/supervisord -c /etc/supervisor.conf
